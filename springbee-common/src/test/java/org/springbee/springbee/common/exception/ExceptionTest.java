6e436c616b7387747b3a741affc1163dc1d20639
Lei Zhang <zhanglei@apache.org>
2020-05-27 15:01:29 +0800

    Fix style

--- springbee-common/src/test/java/org/springbee/springbee/common/exception/ExceptionTest.java
+++ springbee-common/src/test/java/org/springbee/springbee/common/exception/ExceptionTest.java
@@ -4,12 +4,21 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * @author zhanglei
+ */
 public class ExceptionTest {
 
   @Test
   public void testDangerException() {
-    assertThatThrownBy(() -> { throw new DangerException();}).isInstanceOf(DangerException.class);
-    assertThatThrownBy(() -> { throw new DangerException("Oops");}).hasMessage("Oops");
-    assertThatThrownBy(() -> { throw new DangerException("Oops", new RuntimeException());}).hasMessage("Oops");
+    assertThatThrownBy(() -> {
+      throw new DangerException();
+    }).isInstanceOf(DangerException.class);
+    assertThatThrownBy(() -> {
+      throw new DangerException("Oops");
+    }).hasMessage("Oops");
+    assertThatThrownBy(() -> {
+      throw new DangerException("Oops", new RuntimeException());
+    }).hasMessage("Oops");
   }
 }
