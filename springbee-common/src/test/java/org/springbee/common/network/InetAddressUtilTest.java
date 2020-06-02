b6db7ecd55fe0d82083eab5bd9c62457029aea9e
Lei Zhang <zhanglei@apache.org>
2020-06-02 22:10:28 +0800

    Fixed Ipv6 check unit test

--- springbee-common/src/test/java/org/springbee/common/network/InetAddressUtilTest.java
+++ springbee-common/src/test/java/org/springbee/common/network/InetAddressUtilTest.java
@@ -14,12 +14,12 @@ public class InetAddressUtilTest {
   @Test
   public void testIpv6() {
     assertFalse(inetAddressUtil.isIpv6("127.0.0.1"));
-    assertTrue(inetAddressUtil.isIpv6("fe80::aede:48ff:fe00:1122%en5"));
+    assertTrue(inetAddressUtil.isIpv6("2001::85b:3c51:f5ff:ffdb"));
   }
 
   @Test
   public void testConvertAddress() {
     assertEquals(inetAddressUtil.convertAddress("127.0.0.1",8080), "127.0.0.1:8080");
-    assertEquals(inetAddressUtil.convertAddress("fe80::aede:48ff:fe00:1122%en5",8080), "[fe80::aede:48ff:fe00:1122%en5]:8080");
+    assertEquals(inetAddressUtil.convertAddress("2001::85b:3c51:f5ff:ffdb",8080), "[2001::85b:3c51:f5ff:ffdb]:8080");
   }
 }
